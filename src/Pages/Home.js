33b4900a65a10b792d2e117c5e3b26bfd146129d
Dimpho Setsile <vucharity@gmail.com>
2026-02-18 17:18:48 +0200

    added hero section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <>
     
+     <div>
+      <h1>Hero section</h1>
+      <p>Come and enjoy this deliteful cakes </p>
+     </div>
 
     <div className="p-6">
       <h1 className="text-3xl font-bold">
